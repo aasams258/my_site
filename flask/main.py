92ebabf616e2beeca7b8e5e31a9cf01252262d9e
Arthur Sams <arthur@Arthurs-MacBook-Pro.local>
2018-08-01 15:30:33 -0700

    Works

--- flask/main.py
+++ flask/main.py
@@ -17,14 +17,16 @@ import logging
 
 from flask import Flask
 
-
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     return 'Hello World!'
 
+@app.route('/digits')
+def index():
+    return 'Digits'
 
 @app.errorhandler(500)
 def server_error(e):
