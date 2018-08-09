90ea0306fcbe8a5c93ace9b50eb23ff752033b36
Arthur Sams <arthur@Arthurs-MacBook-Pro.local>
2018-08-09 12:41:56 -0700

    Commented out bounding box method

--- flask/main.py
+++ flask/main.py
@@ -31,33 +31,41 @@ def draw():
         dataURI = request.data.decode('UTF-8')
         image_data = re.sub('^data:image/png;base64,', '', dataURI)
         im = Image.open(BytesIO(base64.b64decode(image_data)))
+        '''
+        # If a 1 is bounding boxed, it will explode to a fat line.
+        # Need a better resize tool. maybe just center it on a blank canvas?
+        b_box = im.getbbox()
+        if b_box is None:
+            return jsonify(prediction="Draw something first")
+        im = im.crop(b_box)
+        im = im.resize((28,28), Image.ANTIALIAS)
+        '''
+        '''
+        # For outputing a data URI
+        buffered = BytesIO()
+        im.save(buffered, format="PNG")
+        img_str = base64.b64encode(buffered.getvalue())
+        print(img_str)
+        '''
         im.thumbnail((28,28), Image.ANTIALIAS)
-        #buffered = BytesIO()
-        #im.save(buffered, format="PNG")
-        #img_str = base64.b64encode(buffered.getvalue())
-        # rbgt = 4channels for pixel
-        # Eventually make this output 0 or 1 per RBG value
-        #pixels = list(im.getdata())
         pixels = list(im.getdata())
         # Turn pixels to B&W if they are over $threshold value.
         threshold = 150
         # Color value seems to be stored in the transparency channel.
         # But will summate over all just incase.
-        # Is it possible to have thumbnail set PNG to a different mode?
-        b_w = list(map(lambda rgb: 1 if sum(rgb) >= threshold else 0, pixels))
-        # Now send the array off to the ML tool and return it.
-        #pred = task.make_prediction(b_w)
+        b_w = list(map(lambda rgba: 1 if sum(rgba) >= threshold else 0, pixels))
         data = np.array(b_w)
         data.shape = (28,28)
         data = data.tolist()
+        # Prepare to send to ML Instance
         req = {"instances": [{"x": data}]}
         prediction = mnist_prediction(service, req)
-        print(prediction)
         predicted_class = prediction['predictions'][0]['classes']
         return jsonify(prediction=predicted_class)
     return render_template('digits.html')
     # Potentially add another POST where we add the user corrected result to an
     # "ON DEMAND" First Generation MYSQL instance. Or just regular DB hosted in a bucket?
+    # Also make this not RPC to a ML-Engine, the spin up time is too long.
 
 @app.errorhandler(500)
 def server_error(e):
