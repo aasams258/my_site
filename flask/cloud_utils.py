ac9ace0311e613357b3946b654cd4ed58eb23fab
Arthur Sams <arthur@Arthurs-MacBook-Air.local>
2018-08-31 13:13:53 -0700

    Refactoring

--- flask/cloud_utils.py
+++ flask/cloud_utils.py
@@ -1,24 +1,10 @@
 import os
 
+# To authenticate set the environment variable
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "private/credentials.json"
 
 def mnist_prediction(service, request):
-    """Send json data to a deployed model for prediction.
 
-    Args:
-        project (str): project where the Cloud ML Engine Model is deployed.
-        model (str): model name.
-        instances ([Mapping[str: Any]]): Keys should be the names of Tensors
-            your deployed model expects as inputs. Values should be datatypes
-            convertible to Tensors, or (potentially nested) lists of datatypes
-            convertible to tensors.
-        version: str, version of the model to target.
-    Returns:
-        Mapping[str: any]: dictionary of prediction results defined by the
-            model.
-    """
-    # Create the ML Engine service object.
-    # To authenticate set the environment variable
     name = 'projects/{}/models/{}'.format("my-site-212022", "mnist_model")
     version = None
     if version is not None:
--- flask/main.py
+++ flask/main.py
@@ -1,19 +1,13 @@
 # When running locally install requirements using
 # pip3 install -t lib -r requirements.txt
 # FLASK_APP=main.py FLASK_DEBUG=1 python -m flask run
-
-import logging
-import base64, re
-
+import base64, logging, re, sys
+import numpy as np
 from flask import Flask, jsonify, request, render_template
-import sys
 from io import BytesIO
 from PIL import Image
-
-#Repackage these 2.
-from cloud_utils import mnist_prediction
-import numpy as np
 from googleapiclient import discovery
+from cloud_utils import mnist_prediction
 
 app = Flask(__name__)
 
@@ -23,29 +17,32 @@ service = discovery.build('ml', 'v1')
 def index():
     return render_template('homepage.html')
 
+'''
+The page on LA county health scores.
+'''
 @app.route('/health_scores')
 def health_scores():
     return render_template('health_scores.html')
 
-# {'loss': 0.10061083, 'global_step': 20000, 'accuracy': 0.9701}
+'''
+The page to draw a digit and predict with MNIST layers.
+
+If RPC to ML-Engine spin up time is too long, maybe host model locally.
+'''
 @app.route('/draw', methods=['GET', 'POST'])
 def draw():
     if request.method == 'POST':
-        print("waiting on post")
         dataURI = request.data.decode('UTF-8')
         image_data = re.sub('^data:image/png;base64,', '', dataURI)
         im = Image.open(BytesIO(base64.b64decode(image_data)))
-  
-        # If a 1 is bounding boxed, it will explode to a fat line.
-        # Need a better resize tool. maybe just center it on a blank canvas?
-        # Maybe Check the aspect ratio before cropping? 
-        b_box = im.getbbox()
-        if b_box is None:
+
+        bounding_box = im.getbbox()
+        if bounding_box is None:
             return jsonify(error="Draw something first")
         # Expand bounding box, in order to center the image a bit more.
         expansion_coef = (-20,-20,20,20)
-        b_box2 = [sum(x) for x in zip(b_box,expansion_coef )]
-        im = im.crop(b_box2)
+        enlarged_boundary = [sum(x) for x in zip(bounding_box, expansion_coef )]
+        im = im.crop(enlarged_boundary)
 
         im = im.resize((28,28), Image.ANTIALIAS)
 
@@ -56,28 +53,17 @@ def draw():
         img_str = u'data:img/png;base64,'+ data64.decode('utf-8')
 
         pixels = list(im.getdata())
-        # Turn pixels to B&W if they are over $threshold value.
-        threshold = 150
-        # Color value seems is stored in the transparency channel.
-        #b_w = list(map(lambda rgba: 1 if sum(rgba) >= threshold else 0, pixels))
+        # For the PNG the color is in the alpha channel.
         b_w = list(map(lambda rgba: rgba[3]/255.0, pixels))
         data = np.array(b_w)
         data.shape = (28,28)
         data = data.tolist()
         # Prepare to send to ML Instance
         req = {"instances": [{"x": data}]}
-        prediction = mnist_prediction(service, req)
-        predicted_class = prediction['predictions'][0]['classes']
-        top_3 = sorted(enumerate(prediction['predictions'][0]['probabilities']),key=lambda x: x[1], reverse=True)[0:3]
-        print(top_3[0:3])
-        return jsonify(prediction=top_3, img_uri=img_str)
+        try:
+            prediction = mnist_prediction(service, req)
+            top_3 = sorted(enumerate(prediction['predictions'][0]['probabilities']), key=lambda x: x[1], reverse=True)[0:3]
+            return jsonify(prediction=top_3, img_uri=img_str)
+        except:
+            return jsonify(error="Error in the ML Instance, Please try again.")
     return render_template('digits.html')
-    # Potentially add another POST where we add the user corrected result to an
-    # "ON DEMAND" First Generation MYSQL instance. Or just regular DB hosted in a bucket?
-    # Also make this not RPC to a ML-Engine, the spin up time is too long.
-
-@app.errorhandler(500)
-def server_error(e):
-    # Log the error and stacktrace.
-    logging.exception('An error occurred during a request.')
-    return 'An internal error occurred.', 500
